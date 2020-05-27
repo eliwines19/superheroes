95a3c906e4365b47bbbf7e02adac91c8980341c6
Elijah wines <elijahwines84@yahoo.com>
2020-05-27 03:01:18 +0000

    fixed migration error

--- db/schema.rb
+++ db/schema.rb
@@ -26,7 +26,7 @@ ActiveRecord::Schema.define(version: 2020_05_27_024049) do
 
   create_table "users", force: :cascade do |t|
     t.string "username"
-    t.string "password"
+    t.string "password_digest"
   end
 
 end
