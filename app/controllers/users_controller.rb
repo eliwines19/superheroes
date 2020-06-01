6b566ae64ba1f2d3970b1f3d8c204c65e36c96f1
Elijah wines <elijahwines84@yahoo.com>
2020-06-01 19:38:15 +0000

    got rid of slugs

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,5 @@
 class UsersController < ApplicationController
 
-  get '/users/:slug' do
-      @user = User.find_by_slug(params[:slug])
-      erb :'users/show'
-  end
-
   get '/signup' do
       if !logged_in?
           erb :'/users/new'
@@ -17,7 +12,7 @@ class UsersController < ApplicationController
       if params[:username] == "" || params[:password] == ""
           redirect to '/signup'
       else
-          @user = User.new(params)
+          @user = User.create(:username => params[:username], :password => params[:password])
           @user.save
           session[:user_id] = @user.id
           redirect to '/superheroes'
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,14 +1,6 @@
 class User < ActiveRecord::Base
   has_secure_password
-  has_many :tweets
-
-  def self.find_by_slug(slug)
-    User.all.find {|user| user.slug == slug}
-  end
-
-  def slug
-    username.downcase.gsub(" ","-")
-  end
+  has_many :superheros
 
 end
 
